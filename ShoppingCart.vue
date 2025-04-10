a66e6e93b4c9b03d8d24bc3120a84af51d6d6263
LaddyMih <165798514+LaddyMih@users.noreply.github.com>
2025-04-10 10:42:55 -0300

    Shopping

---
+++ ShoppingCart.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Shopping Cart</h1>
+    <p>Items in cart: {{ cartItemCount }}</p>
+    <button @click="addToCart({ name: 'Product 1', price: 100 })">Add Product 1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    cartItemCount() {
+      return this.$store.getters.cartItemCount;
+    }
+  },
+  methods: {
+    addToCart(product) {
+      this.$store.dispatch('addToCart', product);
+    }
+  }
+}
+</script>
---
+++ index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    cart: [],
+  },
+  mutations: {
+    ADD_TO_CART(state, product) {
+      state.cart.push(product);
+    },
+  },
+  actions: {
+    addToCart({ commit }, product) {
+      commit('ADD_TO_CART', product);
+    },
+  },
+  getters: {
+    cartItemCount: state => {
+      return state.cart.length;
+    },
+  }
+});
